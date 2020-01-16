e77e933bb266c09ab82d4d3aa82824ed446d2a11
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-16 19:45:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point = nil)
   if starting_point
     ret = starting_point
   else 
-    ret = 
+    ret = nil
   end  
   i = 0 
   while i < source_array.length do
