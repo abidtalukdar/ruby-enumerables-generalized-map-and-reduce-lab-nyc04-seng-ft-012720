d46c8ebdac5dd784660b85c366163c198ae39f8f
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-16 19:28:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,9 +2,7 @@ def map(source_array)
   new_arr = []
   i = 0
   while i < source_array.length do 
-    new_arr << yield(
-      
-      source_array[i])
+    new_arr << yield(source_array[i])
   i += 1
   end  
   return new_arr
